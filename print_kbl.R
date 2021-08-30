57475c14539effe155fa3cc0d86f468c0ac10cf7
Kendra Wyant <kpaquette2@wisc.edu>
2021-08-30 13:24:28 -0500

    simplifies update to print kable

--- print_kbl.R
+++ print_kbl.R
@@ -1,20 +1,12 @@
 # Prints a dataframe or other table-like object with nice formatting
 # Defaults to a output box for height = "500px"
 # Might want to use height = "100%" if only printing a few rows
-# Set notes = TRUE if printing large blocks of text in kable table
+# Set align to "l" if printing large blocks of text in kable table
 # this makes the text more readable by adjusting the alignment
 
-print_kbl <- function(data, height = "500px", notes = NULL){
-  
-  if (notes) {
-    data %>%
-      kableExtra::kbl(align = "l") %>%
-      kableExtra::kable_styling(bootstrap_options = c("striped", "condensed")) %>%
-      kableExtra::scroll_box(height = height, width = "100%")
-  } else {
-    data %>%
-      kableExtra::kbl(align = "r") %>%
-      kableExtra::kable_styling(bootstrap_options = c("striped", "condensed")) %>%
-      kableExtra::scroll_box(height = height, width = "100%")
-  }
+print_kbl <- function(data, height = "500px", align = "r"){
+  data %>%
+    kableExtra::kbl(align = align) %>%
+    kableExtra::kable_styling(bootstrap_options = c("striped", "condensed")) %>%
+    kableExtra::scroll_box(height = height, width = "100%")
 }

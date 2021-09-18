256520aa90254f4c6231305c049801bdb015d70e
Kendra Wyant <kpaquette2@wisc.edu>
2021-09-18 08:25:18 -0500

    adds optional parameter for caption title - NULL by default

--- print_kbl.R
+++ print_kbl.R
@@ -5,10 +5,11 @@
 # this makes the text more readable by adjusting the alignment
 # default in this function is to round numeric values to 2 decimal points, adjust 
 # with digits parameter
+# optional parameter to add caption title to kble
 
-print_kbl <- function(data, height = "500px", align = "r", digits = 2){
+print_kbl <- function(data, height = "500px", align = "r", digits = 2, caption = NULL){
   data %>%
-    kableExtra::kbl(align = align, digits = digits) %>%
+    kableExtra::kbl(align = align, digits = digits, caption = caption) %>%
     kableExtra::kable_styling(bootstrap_options = c("striped", "condensed")) %>%
     kableExtra::scroll_box(height = height, width = "100%")
 }

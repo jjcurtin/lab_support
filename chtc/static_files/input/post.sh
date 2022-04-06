a8dc2ccbe7aea6d4225b4888faafb220341460f9
Kendra Wyant <kpaquette2@wisc.edu>
2022-04-06 09:54:44 -0500

    updates training CHTC static files to remove output files from being written out

--- chtc/static_files/input/post.sh
+++ chtc/static_files/input/post.sh
@@ -2,22 +2,17 @@
 # post.sh
 # JJC - will be replaced by a script that transfers raw (not zipped files)
 # For windows, this is a winscp script
+# KW - my files still seem to take a long time to transfer when I don't zip so I may continue to use this script (I need to experiment a little more).
 
 # move data to results
 mv results_*.csv results
 
-#move error files to error
-# mv error*.err error
 
-# move output files to output
-# mv output*.out output
+# OPTIONAL - zip folders
+# zip -r -m results results
+# zip -r -m error error
 
-# put files to transfer into zip files
-zip -r -m results results
-zip -r -m error error
-zip -r -m output output
-
-# move input files into input_files
+# move input files into input_files folder to clean up server 
 mkdir input_files
 mv p*.sh data_trn.rds jobs.csv f*.R sub.sub execute.sh input_files
 

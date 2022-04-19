384b9a67f3b0c0a26b80a3af9110595fc15f49c1
Kendra Wyant <kpaquette2@wisc.edu>
2022-04-19 11:03:37 -0500

    error files now are saved out as err_[jobnum], also adds documentation and commented code to execute file for using staged data

--- chtc/static_files/input/execute.sh
+++ chtc/static_files/input/execute.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # execute.sh
 
+# uncomment code below and edit username and file extension if transferring large file from staging directory - file should already be in staging 
+# Using a file transfer application, like WinSCP, directly drag the large file from its location on your computer to a location within /staging/username/ on transfer.chtc.wisc.edu.
+# IMPORTANT: Also be sure to remove large file name from submit script (under transfer files)
+# IMPORTANT: Your file will need to start with data_trn to be read in correctly in fit script. Remember your file automatically gets renamed to this when mak_jobs is run.
+# Therefore it is recommended you make your jobs and then drag the renamed data_trn file to the transfer server on WinSCP
+# cp /staging/username/data_trn.rds.xz ./
+
 #untar R installation
 tar -xzf R402.tar.gz
 tar -xzf SLIBS.tar.gz
@@ -14,3 +21,7 @@ export R_LIBS=$PWD/packages
 
 #run R script, passing in args
 Rscript fit_chtc.R $1
+
+# uncomment if using large file from staging directory to remove file from woriking directory
+# May need to edit extension on large file (i.e., .csv.xz)
+# rm data_trn.rds.xz

dc9ba18d2515fc7b07228ccb6a11cf47fc1a6a42
jjcurtin <jjcurtin@wisc.edu>
2022-07-07 15:19:12 -0500

    Create rename_files.R

---
+++ rename_files.R
@@ -0,0 +1,16 @@
+# simple script to rename all files in a working directory
+# first set working directory using setwd() or RStudio GUI
+
+library(stringr)
+library(tidyverse)
+
+list.files() # to confirm that the correct files are in directory
+
+names <- tibble(fn = list.files()) %>%   # could restrict list.files() to some pattern if needed
+  separate(fn, c("stem", "ext"), "\\.") %>% 
+  mutate(orig = str_c(stem, ".", ext),
+         new = str_c(stem, "_b2.", ext)) %>% # change b2 as needed
+  glimpse()
+
+# rename files.  Should return TRUE for all files
+file.rename(names$orig, names$new)

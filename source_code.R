99a960679ae365fbe5fbc033939f9e3d128efec3
John Curtin <jjcurtin@wisc.edu>
2023-03-08 09:12:36 -0600

    Create source_code.R

---
+++ source_code.R
@@ -0,0 +1,5 @@
+# EDA------------------
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/fun_eda.R?raw=true")
+
+# print_kbl()---------------
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/print_kbl.R?raw=true")

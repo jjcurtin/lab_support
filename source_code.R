059a0ebc4fafa69fca2b9319521e8105f9b8400e
John Curtin <jjcurtin@wisc.edu>
2023-04-02 11:37:08 -0500

    Update source_code.R

--- source_code.R
+++ source_code.R
@@ -8,4 +8,5 @@ devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/fun_plot
 devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/chtc/static_files/fun_chtc.R?raw=true")
 
 
-
+# ML functions---------
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/fun_ml.R?raw=true")

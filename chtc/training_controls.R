96d3be72971cb2eab7d89ed2adba5f90b49c837c
John Curtin <jjcurtin@wisc.edu>
2023-05-30 10:48:13 -0500

    Update training_controls.R

--- chtc/training_controls.R
+++ chtc/training_controls.R
@@ -1,64 +1,55 @@
-# This is a study level script for defining the recipe and global attributes that are 
-# the same across jobs.
+# Training controls for EMA study
 
-# DO NOT EDIT DIRECTLY IN THIS SCRIPT - this script serves as a template with all 
-# possible parameters specified/demo'd
-
-#EDIT THIS
-study <- "messages"
-data_type <- "all"   # but still need to change more (e.g., feature set) to switch data_type
-window <- "1week"
+# SET GLOBAL PARAMETERS--------------------
+study <- "ema"
+window <- "1hour"
 lead <- 0
-version <- "v1"
-batch <- "batch1" # specify a batch (numbered sequentially) if you need to add to an existing set of jobs (e.g., add hyperparameters but maintain version, algorithm, cv, etc.)
-algorithm <- "glmnet" # specify one algorithm per training control file - can be glmnet, knn, random_forest, xgboost
+version <- "v4"
+algorithm <- "xgboost"
+batch <- "batch5"
 
-
-
-
-
-# SET GLOBAL PARAMETERS --------
 ml_mode <- "classification"   # regression or classification
+configs_per_job <- 50  # number of model configurations that will be fit/evaluated within each CHTC
 
-feature_set <- c("feat_baseline_id", "feat_baseline_temporal") # 1+ feature sets
-data_trn <- str_c("features_", data_type, "_", window, "_", lead, "_", version, ".csv.xz") # set to NULL if using chtc staging for large data
+feature_set <- c("all") # EMA Features set names
+data_trn <- str_c("features_",  window, "_", lead, "_", version, ".csv.xz") 
 
-# 1+ resampling methods (up, down, smote, or none).  All resamples should be in form resample type underscore ratio.  Note that ratio is under_ratio for up and smote and over_ratio for down
-resample <- c("none", "up_1", "down_1", "smote_1", "up_.5", "down_2") 
+seed_splits <- 102030
 
-y_col_name <- "label" # outcome variable - will be changed to y in recipe for consistency across studies 
-y_level_pos <- "" # character string of the outcome variable's positive level (e.g., "yes", "abstinent")
-y_level_neg <- "" # character string of the outcome variable's negative level (e.g., "no", "smoking")
+# OUTCOME-------------------------------------
+y_col_name <- "lapse" 
+y_level_pos <- "yes" 
+y_level_neg <- "no"
 
 
-# CV PARAMETERS
-# All cv parameters must have a value or be set to NULL
-# cv_resample will be used to specify kfold and bootstrapping splits (non-nested)
-# nested cv should use cv_inner_resample and cv_outer_resample instead of cv_resample
-# see resampling demo in lab_support/chtc for examples
+# RESAMPLING FOR OUTCOME-----------------------------------
+# note that ratio is under_ratio for up and smote and over_ratio for down
+resample <- c("down_3") 
+# resample <- c("down_1", "up_1", "smote_1", "down_2", "up_.5", "smote_.5", down_3) 
+
+
+# CV SETTINGS---------------------------------
 cv_resample_type <- "nested" # can be boot, kfold, or nested
 cv_resample = NULL # can be repeats_x_folds (e.g., 1_x_10, 10_x_10) or number of bootstraps (e.g., 100)
-cv_inner_resample <- "1_x_10" # can also be a single number for bootstrapping (i.e., 100)
-cv_outer_resample <- "1_x_10" # outer resample will always be kfold
+cv_inner_resample <- "3_x_10" # can also be a single number for bootstrapping (i.e., 100)
+cv_outer_resample <- "3_x_10" # outer resample will always be kfold
 cv_group <- "subid" # set to NULL if not grouping
-
 
 cv_name <- if_else(cv_resample_type == "nested",
                    str_c(cv_resample_type, "_", cv_inner_resample, "_",
                          cv_outer_resample),
                    str_c(cv_resample_type, "_", cv_resample))
 
-# SET STUDY PATHS
-# the name of the job to set folder names
-name_job <- str_c("train_", window, "_", lead, "_", version, "_", batch, "_", 
-                  algorithm, "_", cv_name) 
-# location of where you want your jobs to be setup
-path_jobs <- str_c("P:/studydata/risk/chtc/", study) 
+# STUDY PATHS----------------------------
+# the name of the batch of jobs to set folder name
+name_batch <- str_c("train_", algorithm, "_", window, "_", cv_name, "_", version, "_", batch) 
+# the path to the batch of jobs to put the folder name
+path_batch <- str_c("P:/studydata/risk/chtc/", study) 
 # location of data set
 path_data <- str_c("P:/studydata/risk/data_processed/", study) 
 
 
-# SET ALGORITHM-SPECIFIC HYPERPARAMETERS
+# ALGORITHM-SPECIFIC HYPERPARAMETERS-----------
 hp1_glmnet <- c(0.05, seq(.1, 1, length.out = 10)) # alpha (mixture)
 hp2_glmnet_min <- -8 # min for penalty grid - will be passed into exp(seq(min, max, length.out = out))
 hp2_glmnet_max <- 2 # max for penalty grid
@@ -72,52 +63,43 @@ hp3_rf <- 1500 # trees (10 x's number of predictors)
 
 hp1_xgboost <- c(0.0001, 0.001, 0.01, 0.1, 0.2, 0.3, .4)  # learn_rate
 hp2_xgboost <- c(1, 2, 3, 4) # tree_depth
-hp3_xgboost <- c(20, 30, 40, 50)  # mtry (previously included 2 and 10 but not needed)
+hp3_xgboost <- c(20, 30, 40, 50)  # mtry
 # trees = 500
 # early stopping = 20
+ 
 
-
-
-# CHANGE CHTC SPECIFIC CONTROLS
-tar <- c("train.tar.gz", "other_project_specific.tar.gz") # name of tar packages for submit file - does not transfer these anywhere 
-max_idle <- 1000 # according to CHTC we should set this at 1000 to not flood the server. It will not limit the number of jobs running at one time 
+# CHTC SPECIFIC CONTROLS----------------------------
+tar <- c("train.tar.gz") # name of tar packages for submit file - does not transfer these anywhere 
+max_idle <- 1000
 request_cpus <- 1 
-request_memory <- "28000MB"
+request_memory <- "25000MB"
 request_disk <- "1600MB"
 flock <- FALSE
 glide <- FALSE
 
-# FORMAT RAW DATA ---------
-# Script should have a single function that classes all variables in the
-# training set as they should be set up prior to recipe
-# Numeric predictors set to numeric, nominal variables (unordered and ordered) set 
-# to factor with levels ordered correctly.  Rename outcome to 'y'.  Can also select
-# out any columns that will not be used for prediction across all training batches.
-# IMPORTANT: This function should be the same for all training batches. 
-# No branching for algorithm, etc.  It is used by post-processing scripts regardless
-# of the best configuration selected.
+# Batches
+# down_1: request_memory <- "24000MB", request_disk <- "1600MB" john
+# down_2: request_memory <- "24000MB", request_disk <- "1600MB" susan
+# up_.5: request_memory <- "28000MB", request_disk <- "1600MB" john (not complete)
+# up_1: request_memory <- "30000MB", request_disk <- "1600MB" kendra (not complete)
+# down_3:
 
+# FORMAT DATA-----------------------------------------
 format_data <- function (df){
-
+  
   df %>% 
     rename(y = !!y_col_name) %>% 
     mutate(y = factor(y, levels = c(!!y_level_pos, !!y_level_neg)), # set pos class first
            across(where(is.character), factor)) %>%
-    select(-label_num, -dttm_label, subid)
+    select(-label_num, -dttm_label)
   # Now include additional mutates to change classes for columns as needed
   # see https://jjcurtin.github.io/dwt/file_and_path_management.html#using-a-separate-mutate
 }
-  
 
-# BUILD RECIPE ---------
 
+# BUILD RECIPE---------------------------------------
 # Script should have a single build_recipe function to be compatible with fit script. 
-# Use if statements to customize recipes instead of using multiple different recipe functions.  
-
-
-# Sample recipe from meta project below - this is for fitting classification models
 build_recipe <- function(d, job) {
-  
   # d: (training) dataset from which to build recipe
   # job: single-row job-specific tibble
   
@@ -137,41 +119,19 @@ build_recipe <- function(d, job) {
     step_zv(all_predictors()) %>% 
     step_impute_median(all_numeric_predictors()) %>% 
     step_impute_mode(all_nominal_predictors()) 
-  
-  # If statements for filtering features based on feature set
-  if (feature_set == "feat_all_passive") {
-    rec <- rec %>%
-      step_rm(starts_with("sms"), -contains("passive")) %>% 
-      step_rm(starts_with("voi"), -contains("passive"))
-  } else if (feature_set == "feat_baseline_id") {
-    rec <- rec %>% 
-      step_rm(starts_with("sms")) %>% 
-      step_rm(starts_with("voi")) %>% 
-      step_rm(starts_with("label"))
-  } else if (feature_set == "feat_baseline_temporal") {
-    rec <- rec %>% 
-      step_rm(starts_with("id")) %>% 
-      step_rm(starts_with("sms")) %>% 
-      step_rm(starts_with("voi")) 
-  } else if (feature_set == "feat_baseline_all") {
-    rec <- rec %>% 
-      step_rm(starts_with("sms")) %>% 
-      step_rm(starts_with("voi")) 
-  } else if (feature_set == "feat_logs") {
-    rec <- rec %>% 
-      step_rm(starts_with("id")) %>% 
-      step_rm(starts_with("label"))
-  }
+
   
   # resampling options for unbalanced outcome variable
   if (resample == "down") {
     rec <- rec %>% 
       themis::step_downsample(y, under_ratio = ratio, seed = 10) 
   }
+  
   if (resample == "smote") {
     rec <- rec %>% 
       themis::step_smote(y, over_ratio = ratio, seed = 10) 
   }
+  
   if (resample == "up") {
     rec <- rec %>% 
       themis::step_upsample(y, over_ratio = ratio, seed = 10)
@@ -193,12 +153,6 @@ build_recipe <- function(d, job) {
       step_dummy(all_nominal_predictors())
   } 
   
-  if (algorithm == "knn") {
-    rec <- rec  %>% 
-      step_dummy(all_nominal_predictors()) %>% 
-      step_normalize(all_predictors())
-  } 
-  
   # final steps for all algorithms
   rec <- rec %>%
     # drop columns with NA values after imputation (100% NA)
@@ -207,5 +161,3 @@ build_recipe <- function(d, job) {
   
   return(rec)
 }
-
-

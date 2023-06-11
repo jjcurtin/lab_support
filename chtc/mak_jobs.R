f81c3ca5c2cf44fa6fbb491d697650ea58e3932e
John Curtin <jjcurtin@wisc.edu>
2023-06-11 09:26:40 -0500

    updates to current

--- chtc/mak_jobs.R
+++ chtc/mak_jobs.R
@@ -8,10 +8,9 @@
 # location of study specific training_controls.R (can use relative path if in repo) 
 path_training_controls <- "./PROJECT/chtc/training_controls.R" 
 
-
 # DON'T CHANGE -------------------- 
-suppressPackageStartupMessages(library(tidyverse))
-source("../lab_support/chtc/fun_make_jobs.R", echo = FALSE)
+library(tidyverse)
+devtools::source_url("https://github.com/jjcurtin/lab_support/blob/main/chtc/fun_make_jobs.R?raw=true")
 
-# set overwrite_jobs to FALSE if you do not want to overwrite existing files
-make_jobs(path_training_controls, overwrite_jobs = TRUE)
+# set overwrite_jobs to TRUE if you want to overwrite existing files.  Be careful!!
+make_jobs(path_training_controls, overwrite_jobs = FALSE)

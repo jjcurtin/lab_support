4d2545256937c4e47b680392dc1ffb9cdf6af1b2
John Curtin <jjcurtin@wisc.edu>
2024-11-04 21:16:49 -0600

    move my_skim to its own script

--- case_analysis.R
+++ case_analysis.R
@@ -1,13 +1,3 @@
-# Creates a customized skimmer with my preferred descriptives
-my_skim <- skimr::skim_with(base = skimr::sfl(n_complete = ~ sum(!is.na(.), 
-                                                                na.rm = TRUE),
-                                              n_missing = ~sum(is.na(.), 
-                                                               na.rm = TRUE)),
-                     numeric = skimr::sfl(p25 = NULL, p75 = NULL, hist = NULL),
-                     character = skimr::sfl(min = NULL, max = NULL),
-                     factor = skimr::sfl(ordered = NULL))
-
-#############################################################################
 # Creates a combined histogram, density plot, and rug plot for x
 # `data` is a tibble
 # `x_name` is a quoted column name

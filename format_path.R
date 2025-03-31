6d15a6b585bb2bef757295d11581280bf1316ac5
John Curtin <jjcurtin@wisc.edu>
2025-03-31 16:03:04 -0500

    update format path

--- format_path.R
+++ format_path.R
@@ -2,25 +2,27 @@
 
 format_path <- function(the_path, resource = "standard"){
 
-  # UDATE TO ONLY HAVE RESTRICTED MOUNTED FOR LINUX AND WINDOWS
   # standard is standard research drive (S drive for windows)
   # restricted is restricted research drive (R drive for windows)
   
-  if(!(resource == "standard" | resource == "restricted" | resource == "private")){
-    stop("Error: resource must be either 'standard', 'restricted', or 'private'")
+  os <- Sys.info()[['sysname']]
+  
+  if(!(resource == "standard" | resource == "restricted")){
+    stop("Error: resource must be either 'standard' or 'restricted'")
+  }
+  if(resource == "restricted" &&  os == "Darwin"){
+    stop("Error: resource must be 'standard' for Mac OS")
   }
   
   switch (Sys.info()[['sysname']],
           
           # PC paths
           Windows = {the_path <- dplyr::if_else(resource == "standard", 
-                                                stringr::str_c("P:/", the_path),
+                                                stringr::str_c("S:/", the_path),
                                                 stringr::str_c("R:/", the_path))},        
   
-          # IOS paths
-          Darwin = {the_path <- dplyr::if_else(resource == "standard",
-                                               stringr::str_c("/Volumes/jjcurtin/studydata/", the_path),
-                                               stringr::str_c("/Volumes/restricted/", the_path))},
+          # IOS path
+          Darwin = {the_path <- stringr::str_c("/Volumes/jjcurtin/studydata/", the_path)},
           
           # Linux paths
           Linux = {the_path <- dplyr::if_else(resource == "standard",
@@ -28,5 +30,4 @@ format_path <- function(the_path, resource = "standard"){
                                               stringr::str_c("~/mnt/restricted/", the_path))},
   )
   return(the_path)
-  
 }
